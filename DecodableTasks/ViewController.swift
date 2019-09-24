7a3023a260ec689b6e0e030dfc28cf9c8ba3b807
mohamedosama2074 <iosengks74@gmail.com>
2019-09-24 03:26:36 +0200

    DecodableExample

--- DecodableTasks/ViewController.swift
+++ DecodableTasks/ViewController.swift
@@ -10,11 +10,120 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let url:String = "https://api.letsbuildthatapp.com/jsondecodable/course"
+    
+    let url2:String = "https://api.letsbuildthatapp.com/jsondecodable/courses"
+    
+    let url3:String = "https://api.letsbuildthatapp.com/jsondecodable/website_description"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+       // simpleExampleLowLevel(url)
+        
+        //simpleExampleArrayLevel(url2)
+        
+        midumExampleArrayLevel(url3)
+
+}
+
+
+struct Courses:Decodable{
+    
+    var id:Int
+    var name:String
+    var link:String
+    var imageUrl:String
+    
+    
+}
+    
+    struct User:Decodable{
+        
+        var name:String
+        var description:String
+        var courses:[Courses]
+        
     }
 
-
+    func simpleExampleLowLevel(_ url:String){
+        
+        guard let url = URL(string: url) else {return}
+        
+        
+        URLSession.shared.dataTask(with: url) { (data, rseponse, error) in
+            
+            do{
+                
+                let jsonDecoder = try JSONDecoder().decode(Courses.self
+                    , from: data!)
+                
+                print(jsonDecoder.name)
+                print(jsonDecoder.id)
+                print(jsonDecoder.imageUrl)
+                
+            }catch let error{
+                
+                print(error)
+            }
+            }.resume()
+        
+        
+        
+    }
+    
+    func simpleExampleArrayLevel(_ url:String){
+        
+        guard let url = URL(string: url) else {return}
+        
+        
+        URLSession.shared.dataTask(with: url) { (data, rseponse, error) in
+            
+            do{
+                
+                let jsonDecoder = try JSONDecoder().decode([Courses].self
+                    , from: data!)
+                
+                for i in jsonDecoder{
+                    
+                    print(i.name)
+                }
+                
+            }catch let error{
+                
+                print(error)
+            }
+            }.resume()
+        
+        
+        
+    }
+    
+    func midumExampleArrayLevel(_ url:String){
+        
+        guard let url = URL(string: url) else {return}
+        
+        
+        URLSession.shared.dataTask(with: url) { (data, rseponse, error) in
+            
+            do{
+                
+                let jsonDecoder = try JSONDecoder().decode(User.self
+                    , from: data!)
+                
+                print(jsonDecoder.name , jsonDecoder.description)
+                
+                
+            }catch let error{
+                
+                print(error)
+            }
+            }.resume()
+        
+        
+    }
+    
+  
+        
 }
 
